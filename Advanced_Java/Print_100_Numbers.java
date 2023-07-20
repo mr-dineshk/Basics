3e6dc2defff595f93c1578ceac2ce85335733b2c
Dinesh Kumar G <113843798+mr-dineshk@users.noreply.github.com>
2023-07-20 09:21:20 +0530

    Create Print_100_Numbers.java

---
+++ Advanced_Java/Print_100_Numbers.java
@@ -0,0 +1,13 @@
+//a program to print all numbers from 1 to 100 i.e. 1 2 3 4 5 6 7 . . . 98 99 100
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main (String[] args){
+        int i = 1;
+        while (i < 101){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
