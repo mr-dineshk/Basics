63e672078ca7553e01a94fd7a7e404d8a692f044
Dinesh Kumar G <113843798+mr-dineshk@users.noreply.github.com>
2023-07-25 13:36:09 +0530

    Update LargestElementArray.java

--- Advanced_Java/Day-12/LargestElementArray.java
+++ Advanced_Java/Day-12/LargestElementArray.java
@@ -1,10 +1,17 @@
 import java.util.Scanner;
-public class LargestElementArray {
-
-	public static void main(String[] args) {
-		//Scanner scan = new Scanner(System.in);
+public class LargestElementArray 
+{
+	public static void main(String[] args) 
+	{
+		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter the no of Array: ");
-		int a[] = {12,13,99,12,34};
+		int n = scan.nextInt();
+		int a[] = new int[n];
+		System.out.print("Enter the elements of array: ");
+		for (int i = 0; i<n; i++)
+		{
+		    a[i] = scan.nextInt();    
+		}
 		int max = a[0];
 		for (int i = 0; i<a.length; i++) 
 		{
@@ -14,7 +21,7 @@ public class LargestElementArray {
 			}
 			
 		}
-		System.out.println(max+" ");
+		System.out.println("Maximum element is "+max);
 
 	}
 
